760daf37c9621c1d0c846405c0ec6d060806ea8d
Emme Buentiempo <emmettbuentiempo@gmail.com>
2020-12-09 16:01:22 -0800

    save 1.
    co-authored-by: Natalie Murphy <NatalieMurphy500@gmail.com>
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

---
+++ src/apps.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Actor from "./components/actor";
+
+export default function App() {
+  const data = {
+    h: 32,
+    w: 32,
+  };
+  return (
+    <div className="zone-container">
+      <Actor sprite={`/sprites/skin/m1.png`} data={data} />
+    </div>
+  );
+}
---
+++ src/components/player/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Actor from "../actor"
+import useKeyPress from "../../hooks/use-key-press"
+import useWalk from "../../hooks/use-walk"
+
+export default function Player({skin}){
+  const {dir, step, walk} = useWalk(3)
+    const data= {
+        h:32,
+        w:32,
+      };
+      
+
+      useKeyPress((e)=> {
+        const dir= e.key.replace("Arrow","").toLowerCase();
+        walk(dir);
+        // if(directions.hasOwnProperty(dir))console.dir(dir);
+        e.preventDefault();
+      })
+    return <Actor sprite = {`/sprites/skins/${skin}.png`} data={data}/>;
+}
